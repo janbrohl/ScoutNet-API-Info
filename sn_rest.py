d7f6ffd56ef488147b8abd662f04b803ab0755f9
Jan Brohl <janbrohl@t-online.de>
2017-01-13 15:17:58 +0100

    format + typo

--- sn_rest.py
+++ sn_rest.py
@@ -2,9 +2,17 @@ import json
 import requests
 
 
+SN_REST_BASE_URL = "https://www.scoutnet.de/api/0.2/"
+
+
 def sn_rest(path, *args):
     if args:
-        params = {"json": json.dumps(args)}
+        params = {
+            "json": json.dumps(args)
+        }
     else:
         params = {}
-    return requests.get(SN_REST_BASE_URL + path, params=params).json()
+    return requests.get(
+        SN_REST_BASE_URL + path,
+        params=params
+    ).json()
--- sn_rpc.py
+++ sn_rpc.py
@@ -14,8 +14,15 @@ def rpc(url, method, *params):
     JSON-RPC 1.0 über HTTP(S) POST
     """
     call_id = os.urandom(16).hex()
-    json_data = {"method": method, "params": params, "id": call_id}
-    got = requests.post(url, json=json_data).json()
+    json_data = {
+        "method": method,
+        "params": params,
+        "id": call_id
+    }
+    got = requests.post(
+        url,
+        json=json_data
+    ).json()
     e = got["error"]
     if e is not None:
         raise RPCError(e)
@@ -23,4 +30,8 @@ def rpc(url, method, *params):
 
 
 def sn_rpc_get(*params):
-    return rpc(SN_RPC_URL, "get_data_by_global_id", *params)
+    return rpc(
+        SN_RPC_URL,
+        "get_data_by_global_id",
+        *params
+    )
