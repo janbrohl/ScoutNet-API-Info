93afec44a83eefbb1a741e36d54061e27cac06a2
Jan Brohl <janbrohl@t-online.de>
2017-01-21 20:47:21 +0100

    vollständigerer sn-rpc beispielcode

--- sn_rpc.py
+++ sn_rpc.py
@@ -1,8 +1,15 @@
 import os
+import json
+import hashlib
+import time
+import base64
 import requests
+import Crypto.Cipher.AES
 
 
 SN_RPC_URL = "https://www.scoutnet.de/jsonrpc/server.php"
+
+SN_RPC_IV = b'1234567890123456'
 
 
 class RPCError(Exception):
@@ -29,9 +36,78 @@ def rpc(url, method, *params):
     return got["result"]
 
 
-def sn_rpc_get(*params):
+def get_data_by_global_id(global_id, query={}):
     return rpc(
         SN_RPC_URL,
         "get_data_by_global_id",
-        *params
+        global_id,
+        query
     )
+
+
+def checkPermission(type, global_id, username, auth):
+    return rpc(
+        SN_RPC_URL,
+        "checkPermission",
+        type,
+        global_id,
+        username,
+        auth
+    )
+
+
+def requestPermission(type, global_id, username, auth):
+    return rpc(
+        SN_RPC_URL,
+        "requestPermission",
+        type,
+        global_id,
+        username,
+        auth
+    )
+
+
+def setData(type, id, object, username, auth):
+    return rpc(
+        SN_RPC_URL,
+        "requestPermission",
+        type,
+        id,
+        object,
+        username,
+        auth
+    )
+
+
+def deleteObject(type, global_id, id, username, auth):
+    return rpc(
+        SN_RPC_URL,
+        "requestPermission",
+        type,
+        global_id,
+        id,
+        username,
+        auth
+    )
+
+
+def generate_auth(api_key, check_value):
+    auth = {
+        "sha1": hashlib.sha1(check_value).hexdigest(),
+        "md5": hashlib.md5(check_value).hexdigest(),
+        "time": time.time()
+    }
+    auth = json.dumps(auth).encode("ascii")
+    first_block = os.urandom(16)
+    aes = Crypto.Cipher.AES.new(api_key, Crypto.Cipher.AES.MODE_CBC, SN_RPC_IV)
+    blocks = first_block + auth
+    blocks = blocks + bytes(16 - len(blocks) % 16)  # padding
+    out = aes.encrypt(blocks)
+    out = base64.b64encode(out)
+    for a, b in ((b'+', b'-'), (b'/', b'_'), (b'=', b'~')):
+        out = out.replace(a, b)
+    return out
+
+
+def generate_check_value(*args):
+    return ("".join(str(p) for p in args)).encode("ascii")
