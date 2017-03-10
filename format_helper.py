bbab64b6f479d5373958c2d1a3256fc4f0361f13
Jan Brohl <janbrohl@t-online.de>
2017-03-10 13:51:23 +0100

    format_helper.py schreibt nur noch geänderte dateien neu

--- format_helper.py
+++ format_helper.py
@@ -1,21 +1,26 @@
 import glob
 import json
 
-#format
+# format
 for fn in glob.iglob("schemas/*/*.json"):
-    with open(fn,encoding="utf8") as f:
-        obj=json.load(f)
-    with open(fn,"w",encoding="utf8") as f:
-        json.dump(obj,f, indent=1, sort_keys=True)
+    with open(fn, encoding="utf8") as f:
+        so = f.read()
+    obj = json.loads(so)
+    sn = json.dumps(obj, indent=1, sort_keys=True)
+    if sn != so:
+        with open(fn, "w", encoding="utf8") as f:
+            f.write(sn)
 
-#format
+
+# format
 for fn in glob.iglob("docs/**/*.rst"):
-    with open(fn,encoding="utf8") as f:
-        lines=[l.rstrip().replace("\t","    ") for l in f]
-    with open(fn,"w",encoding="utf8",newline="") as f:
-        for l in lines:
-            f.write(l)
-            f.write("\n")
-        if lines[-1]:
-            f.write("\n")
-        
+    with open(fn, encoding="utf8", newline="") as f:
+        so = f.read()
+    lines = [l.rstrip().replace("\t", "    ") for l in so.splitlines()]
+    if lines[-1]:
+        lines.append("")
+    lines.append("")
+    sn = "\n".join(lines)
+    if sn != so:
+        with open(fn, "w", encoding="utf8", newline="") as f:
+            f.write(sn)
