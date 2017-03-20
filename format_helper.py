c3495703628ce04b4bf5ea5db80969e6172294bf
Jan Brohl <janbrohl@t-online.de>
2017-03-20 19:15:45 +0100

    keine neusortierung der schemas

--- format_helper.py
+++ format_helper.py
@@ -1,12 +1,13 @@
 import glob
 import json
+import collections
 
 # format
 for fn in glob.iglob("schemas/*/*.json"):
     with open(fn, encoding="utf8") as f:
         so = f.read()
-    obj = json.loads(so)
-    sn = json.dumps(obj, indent=1, sort_keys=True)
+    obj = json.loads(so, object_pairs_hook=collections.OrderedDict)
+    sn = json.dumps(obj, indent=1)
     if sn != so:
         with open(fn, "w", encoding="utf8") as f:
             f.write(sn)
